038d3ac999d422a99b7e355d36e37cd1aca42dd7
Hezekiah Adeusi <hezekiaha58@gmail.com>
2023-11-27 17:21:55 +0000

    Improved list item removal by attaching dedicated delete buttons

--- script.js
+++ script.js
@@ -9,9 +9,20 @@ enter.addEventListener("click", (event) => {
     const newItem = document.createElement('li');
     newItem.innerText = input.value;
 
+    // Create and append a unique "Delete" button for this list item
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.addEventListener('click', () => {
+        newItem.parentNode.removeChild(newItem); // Remove the corresponding list item
+    });
+
+    // Append both the list item and its associated delete button
+    newItem.appendChild(deleteButton);
     toDoList.appendChild(newItem);
 })
 
-remove.innerHTML = "Delete";
-todoForm.appendChild(remove);
+
+
+
+
 
