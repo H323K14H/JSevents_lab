a4ad038b07cd1a828284b920f2d823306d331b93
Gisele <giselec12@Giseles-MacBook-Air.local>
2023-11-27 15:05:46 +0000

    Added two new DOMs into the script.js file

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const enter = document.querySelector('#enter');
 const toDoList = document.querySelector('#list');
+const input = document.querySelector('#new-todo')
+const list = document.querySelector('#list')
 
 enter.addEventListener("click", () => {
     console.log("Clicked!")
 })
+
+
+
